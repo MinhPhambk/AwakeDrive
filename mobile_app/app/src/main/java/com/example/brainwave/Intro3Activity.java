8fb6a03e1021274b84e6f8e7af4dfa6d338a95aa
MinhPhambk <phamngocminh1230@gmail.com>
2024-03-04 15:14:56 +0700

    Save file from local

---
+++ mobile_app/app/src/main/java/com/example/brainwave/Intro3Activity.java
@@ -0,0 +1,37 @@
+package com.example.brainwave;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Intro3Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.intro3_view);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPostResume() {
+        super.onPostResume();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+}
